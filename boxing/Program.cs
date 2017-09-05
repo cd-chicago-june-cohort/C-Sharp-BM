90349bdd58467957fde8313b3c03a6357c622d44
Bald Mike <baldmike312@gmail.com>
2017-09-05 18:09:31 -0500

    box it up

---
+++ boxing/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            List<object> things = new List<object>();
+
+            things.Add(7);
+            things.Add(28);
+            things.Add(-1);
+            things.Add(true);
+            things.Add("chair");
+
+            for (int i = 0; i < things.Count; i++) {
+            Console.WriteLine(things[i]);
+            }
+
+            int sum = 0;
+            foreach (var thing in things) {
+            if (thing is int) {
+                sum += (int)thing;
+            }
+
+            } Console.WriteLine(value: "The sum of the things is " + sum);
+        }
+    }
+}
